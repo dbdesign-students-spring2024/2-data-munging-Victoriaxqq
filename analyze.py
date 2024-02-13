e57f6da27fede4aa4685635bd86a9b031fad56b6
Victoriaxqq <157643752+Victoriaxqq@users.noreply.github.com>
2024-02-12 21:11:55 -0500

    finished

--- analyze.py
+++ analyze.py
@@ -1,1 +1,68 @@
 # Place code below to do the analysis part of the assignment.
+
+import csv
+import os
+
+platform_agnostic_file_path = os.path.join('data', 'clean_data.csv')
+csv_reader = csv.DictReader(open(platform_agnostic_file_path, 'r'))
+averages_list = []
+
+# Convert the CSV values to a list for easier processing
+data = list(csv_reader)
+
+#Calculate the number of remainder years for decades
+remainder = len(data)%10
+
+# build a list to store the year
+year = []
+new_year = []
+for value in data:
+    year += [value['Year']]
+for i in range(0, len(year), 10):
+    new_year += [year[i]]
+
+
+
+# Iterate through each column in the CSV file
+for column_name in csv_reader.fieldnames[1:-1]:
+    # Calculate the average of every 10 values in one column without overlapping
+    values_to_add = [float(row[column_name]) for row in data]
+    column_averages = []
+
+    for i in range(0, len(values_to_add) - remainder, 10):
+        avg = sum(values_to_add[i:i+10]) / min(10, len(values_to_add) - i)
+        column_averages.append(float(format(avg, '.2f')))
+
+        # Build a dictionary to store the result
+        dic = {}
+        for i in range(len(column_averages)):
+            dic[f"{new_year[i]}s"] = column_averages[i]
+
+ 
+    averages_list.append({column_name: column_averages})
+
+# Calculate the annual average
+first_12_rows = averages_list[:12]  # Select the first 12 rows
+
+annual_decades = []
+
+for i in range(len(column_averages)):
+    column_sum = 0
+    for element in first_12_rows:
+        for value in element.values():
+            column_sum += value[i]
+    annual_decades.append(float(format(column_sum/12, '.2f')))
+dic2 = {}
+for i in range(len(column_averages)):
+    dic2[f"{new_year[i]}s"] = annual_decades[i]
+
+# print the annual mean
+print('Average Temperature Difference by Decade (deg-F)\n')
+print(f"Annual Average: {dic2}\n")
+
+
+
+# Print the averages for each column in list format
+for column_data in averages_list:
+    for column_name, column_avg in column_data.items():
+        print(f"Averages for {column_name}: {dic}\n")
--- munge.py
+++ munge.py
@@ -1,1 +1,73 @@
 # Place code below to do the munging part of this assignment.
+
+# Read the raw data file 
+import os
+platform_agnostic_file_path = os.path.join('data', 'clean_data.csv')
+with open(platform_agnostic_file_path, 'r')as file:
+    lines = file.readlines()
+
+
+# List to store the cleaned data
+clean_data = []
+
+
+# Finding the header row of the table
+for line in lines:
+    if line.startswith(' ') or line == '\n':
+        continue
+    line = line.strip()
+
+    # Using ',' to replace space in the header row to make it clear in csv format
+    tokens = [token for token in line.split() if token] 
+    line = ",".join(tokens)
+
+    # Adding header row in clean_data as header row
+    clean_data.append(line)
+    break
+
+
+for line in lines:
+
+    # Moving the line with notes and repeated header row
+    if line[0].isnumeric():
+
+        # Handle the missing data by deleting the whole row, 
+        # since the number of the data with missing value is relatively smaller 
+        # than the number of the whole data
+        if '*' not in line:
+
+            # Convert temperature anomalies to Farenheit (deviding 100 and then multiply that result by 1.8(=9/5))
+            temp = line.split()
+            
+            # Build a string to store cleaned value
+            temp_new = ''
+
+            for i in range(len(temp)):
+
+                # handle all data exclude year
+                if i in range(1, len(temp)-1):
+            
+                    new = ((float(temp[i]))/100)*1.8
+                    
+                    # format the data 
+                    temp[i]  = str(format(new, ".1f"))
+
+                    # Using ',' to split data which can make data more clear in csv format
+                    temp_new += (temp[i] +',')
+                
+                # handle year
+                else:
+                    # Using ',' to split data which can make data more clear in csv format
+                    temp_new += (temp[i] +',')
+
+
+            clean_data.append(temp_new)
+
+# Save the data in clean_data.csv in data file
+with open('/Users/quanquanxie/Documents/GitHub/2-data-munging-Victoriaxqq/data/clean_data.csv', 'w') as f:
+    f.write('\n'.join(clean_data))
+
+
+
+
+
